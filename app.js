f60ade6038ae3d68316da9e5f43a966c5b4e7c53
tommy <tcatty7@gmail.com>
2023-05-30 17:37:23 -0400

    done

--- app.js
+++ app.js
@@ -1,26 +1,67 @@
+let workTitle = document.getElementById("work");
+let breakTitle = document.getElementById("break");
+let e = document.getElementById("input").value
+let workTime = e;
+let breakTime = 5;
+let seconds = "00";
 
-function todolist(){
-    let text = document.getElementById("text");
-var t = document.createTextNode(text.value)
-var v = document.createTextNode(text.value)
-var newitem = document.createElement("li")
-var checked = document.createElement("li")
-var remove = document.createElement("button")
-var complete = document.createElement("button")
-newitem.append(t, remove, complete)
-checked.append(v)
-document.getElementById("todolist").appendChild(newitem)
-text.value = ""
-remove.innerText = "Remove"
-complete.innerText = "completed"
-remove.addEventListener('click', function(){
-    document.getElementById("todolist").removeChild(newitem)
-})
-complete.addEventListener('click', function(){
-    document.getElementById("completed").appendChild(checked)
-    document.getElementById("todolist").removeChild(newitem)
-})
+window.onload = () => {
 
+    document.getElementById("minutes").innerHTML = workTime;
+    document.getElementById("seconds").innerHTML = seconds;
 
+    workTitle.classList.add('active');
 }
+
+function start(){
+
+document.getElementById("start").style.display = "none"
+let reset = document.getElementById("reset")
+reset.style.display = "inherit"
+
+    seconds = 59;
+
+    let workMinuets = workTime - 1;
+    let breakMinuets = breakTime - 1;
+
+    breakCount = 0;
+
+reset.addEventListener("click", () => {
+    document.getElementById("minutes").innerHTML = workTime;
+    document.getElementById("seconds").innerHTML = "00";
+    reset.style.display = "none"
+    document.getElementById("start").style.display = "inherit"
+    clearInterval(a)
+})
+
+    let timerFunction = () => {
+        document.getElementById("minutes").innerHTML = workMinuets;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        seconds = seconds - 1;
+
+        if(seconds === 0) {
+            workMinuets = workMinuets - 1;
+            if(workMinuets === -1){
+            if(breakCount % 2 === 0){
+                workMinuets = breakMinuets;
+                breakCount++
+
+                workTitle.classList.remove("active")
+                breakTitle.classList.add("active")
+            }else{
+                workMinuets = workTime;
+                breakCount++
+
+                workTitle.classList.add("active")
+                breakTitle.classList.remove("active")
+            }
+            }
+            seconds = 59;
+        }
+
+    }
+  let a =   setInterval(timerFunction, 1000);
+}
+
 
